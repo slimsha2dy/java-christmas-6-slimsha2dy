0ce7181ba0c928124c3b3eedf2b1bbfada3db991
최휘용 <chy2581@naver.com>
2023-11-13 18:30:16 +0900

    test: 주문 예외 처리 테스트
    Utility의 orderParser가 String을 잘 파싱하고 중복 예외를 잘 처리하는 확인하는 UtilityTest 구현
    validateOrder, validateFormat이 다양한 예외를 잘 처리하는지 확인하는 함수를 ValidatorTest에 추가

--- src/test/java/christmas/ValidatorTest.java
+++ src/test/java/christmas/ValidatorTest.java
@@ -32,4 +32,50 @@ public class ValidatorTest {
         assertThatThrownBy(() -> Validator.validateDate("123"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @DisplayName("입력된 주문이 비어있으면 예외가 발생한다.")
+    @Test
+    void isOrderNoneTest() {
+        assertThatThrownBy(() -> Validator.validateOrder(""))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("입력된 주문 중 빈 문자열이 있으면 예외가 발생한다.")
+    @Test
+    void isParsedNoneTest() {
+        assertThatThrownBy(() -> Validator.validateOrder("해산물파스타-2,레드와인-1,"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Validator.validateOrder(",해산물파스타-2,레드와인-1"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Validator.validateOrder("해산물파스타-2,레드와인-1,"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Validator.validateOrder("해산물파스타-2,,레드와인-1"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Validator.validateOrder(",,,"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문 포맷이 올바르지 않으면 예외가 발생한다.")
+    @Test
+    void isFormatWrongTest() {
+        assertThatThrownBy(() -> Validator.validateOrder("해산물파스타"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Validator.validateOrder("해산물파스타-1-1,제로콜라-1"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Validator.validateOrder("해산물파스타-1,제로콜라-"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Validator.validateOrder("해산물파스타-1,---"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("개수에 숫자가 아닌 값이 들어가면 예외가 발생한다.")
+    @Test
+    void isNotNumberTest() {
+        assertThatThrownBy(() -> Validator.validateOrder("해산물파스타-레드와인"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Validator.validateOrder("해산물파스타-1,제로콜라-레드와인"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Validator.validateOrder("해산물파스타-1,제로콜라-2레드와인"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
