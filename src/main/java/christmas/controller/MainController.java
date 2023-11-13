b1db40155a858f331cfa4c79da868d711dec4938
최휘용 <chy2581@naver.com>
2023-11-13 16:06:56 +0900

    feat: MainController 구현
    코드의 전체 흐름을 책임지는 MainController 클래스 와 run 메서드 구현

---
+++ src/main/java/christmas/controller/MainController.java
@@ -0,0 +1,13 @@
+package christmas.controller;
+
+import christmas.view.*;
+
+public class MainController {
+
+    public MainController() {
+    }
+
+    public void run() {
+        int date = InputView.readDate();
+    }
+}
